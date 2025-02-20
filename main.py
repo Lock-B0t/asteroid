5326a55859d74abe5d409f7e626f9f7b30184abd
Lock-B0t <lck62064@gmail.com>
2025-02-20 13:14:09 -0500

    B: Added in displaying the player on the screen.

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import pygame
 import constants as con
+import player as ply
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+
     print("Starting Asteroids!")
     print(f"Screen width: {con.SCREEN_WIDTH}")
     print(f"Screen height: {con.SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((con.SCREEN_WIDTH, con.SCREEN_WIDTH))
+    player = ply.Player(con.SCREEN_WIDTH / 2, con.SCREEN_HEIGHT / 2)
+
+
 
     while True:
         for event in pygame.event.get():
@@ -14,9 +21,10 @@ def main():
                 return
     
         screen.fill("black")
-
+        player.draw(screen)
 
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 
